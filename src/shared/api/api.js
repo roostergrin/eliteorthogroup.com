a5b3bd80bee1adc59fc7bb03a31ae42aa1467e3b
spragala <me@spragala.com>
2019-01-06 19:26:53 -0800

    fix(grid): adjusts grid for project

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -2,7 +2,7 @@ const route = '//eliteorthogroup.roostertest2.com/wp-json'
 
 const api = () => {
   try {
-    return (document.location.protocol === 'https:' ? 'https://ssl' : 'http://') + route
+    return (document.location.protocol === 'https:' ? 'https:' : 'http:') + route
   } catch (e) {
     return 'https://' + route
   }
