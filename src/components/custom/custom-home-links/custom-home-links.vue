e617dba29b4cdab0b93d16f7fa1f1c3e04d0e437
spragala <me@spragala.com>
2019-01-19 20:48:08 -0800

    refactor(home/links): adds animation and mobile styling to home links section

--- src/components/custom/custom-home-links/custom-home-links.vue
+++ src/components/custom/custom-home-links/custom-home-links.vue
@@ -2,6 +2,31 @@
 
 <script>
 export default {
-  props: ['props']
+  data () {
+    return {
+      active: false,
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.25]
+      }
+    }
+  },
+  computed: {
+    links () {
+      return this.$refs.links
+    }
+  },
+  props: ['props'],
+  methods: {
+    onScroll ({going}) {
+      if (going === 'in') {
+        this.active = true
+        this.links.forEach(function (el, i) {
+          setTimeout(() => { el.classList.add('custom-home-links__link--active') }, (i + 1) * 350)
+        })
+      }
+    }
+  }
 }
 </script>
