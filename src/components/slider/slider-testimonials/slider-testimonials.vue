f5803ab82d5c032512c3a9f909a25952c1d7ab57
spragala <me@spragala.com>
2019-01-21 10:46:49 -0800

    feat(testimonials): adds testimonial slider to home page

---
+++ src/components/slider/slider-testimonials/slider-testimonials.vue
@@ -0,0 +1,37 @@
+<template lang="pug" src="./slider-testimonials.pug"></template>
+
+<script>
+import { onScroll } from 'resources/mixins'
+export default {
+  data () {
+    return {
+      show: 0,
+      num: 1,
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.5]
+      }
+    }
+  },
+  props: ['props'],
+  mixins: [onScroll],
+  async created () {
+    setInterval(() => {
+      if (this.show === (this.props.slides.length - 1)) {
+        this.show = 0
+        this.num = 1
+      } else {
+        this.show++
+        this.num++
+      }
+    }, 8000)
+  },
+  methods: {
+    moveSlide (i) {
+      this.show = i
+      this.num = (i + 1)
+    }
+  }
+}
+</script>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -4,6 +4,7 @@
 import HeroHome from 'components/hero/hero-home/hero-home'
 import Invisalign from 'custom/custom-home-invisalign/custom-home-invisalign'
 import Links from 'custom/custom-home-links/custom-home-links'
+import Testimonials from 'components/slider/slider-testimonials/slider-testimonials'
 import Welcome from 'custom/custom-home-welcome/custom-home-welcome'
 
 export default {
@@ -17,6 +18,7 @@ export default {
     HeroHome,
     Invisalign,
     Links,
+    Testimonials,
     Welcome
   }
 }
