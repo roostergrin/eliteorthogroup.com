cfca71a33a28ce05addde5fbcc47ed8b2d5080fb
spragala <me@spragala.com>
2019-01-23 10:28:12 -0800

    feat(braces/types): adds types of braces component

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -20,12 +20,12 @@ export default {
     },
     handleClick (i) {
       if (i === 'loc') {
-        this.$refs.phone.classList.remove('nav-desktop__item--active')
-        this.$refs.location.classList.contains('nav-desktop__item--active') ? this.$refs.location.classList.remove('nav-desktop__item--active') : this.$refs.location.classList.add('nav-desktop__item--active')
+        this.$refs.phone.classList.remove('navigation__item--active')
+        this.$refs.location.classList.contains('navigation__item--active') ? this.$refs.location.classList.remove('navigation__item--active') : this.$refs.location.classList.add('navigation__item--active')
       }
       if (i === 'phone') {
-        this.$refs.location.classList.remove('nav-desktop__item--active')
-        this.$refs.phone.classList.contains('nav-desktop__item--active') ? this.$refs.phone.classList.remove('nav-desktop__item--active') : this.$refs.phone.classList.add('nav-desktop__item--active')
+        this.$refs.location.classList.remove('navigation__item--active')
+        this.$refs.phone.classList.contains('navigation__item--active') ? this.$refs.phone.classList.remove('navigation__item--active') : this.$refs.phone.classList.add('navigation__item--active')
       }
     }
   }
