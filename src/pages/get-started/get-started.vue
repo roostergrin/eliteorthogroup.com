3c6649f2a563c5013d256f005de709429b47e65f
spragala <me@spragala.com>
2019-01-25 11:03:11 -0800

    feat(divider): adds simple text divider to get started page

--- src/pages/get-started/get-started.vue
+++ src/pages/get-started/get-started.vue
@@ -1,6 +1,7 @@
 <template lang="pug" src="./get-started.pug"></template>
 
 <script>
+import Divider from 'components/divider/divider-text/divider-text'
 import Hero from 'components/hero/hero-pages/hero-pages'
 
 export default {
@@ -10,6 +11,7 @@ export default {
     }
   },
   components: {
+    Divider,
     Hero
   }
 }
