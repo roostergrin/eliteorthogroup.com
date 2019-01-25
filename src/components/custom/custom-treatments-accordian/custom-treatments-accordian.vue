23e5b14d80a416e47d63a9041bf3f445105e35e1
spragala <me@spragala.com>
2019-01-24 21:37:09 -0800

    feat(treatments): adds accordion section to treatments page

---
+++ src/components/custom/custom-treatments-accordian/custom-treatments-accordian.vue
@@ -0,0 +1,32 @@
+<template lang="pug" src="./custom-treatments-accordian.pug"></template>
+
+<script>
+import { onScroll } from 'resources/mixins'
+
+export default {
+  data () {
+    return {
+      activeTab: null,
+      open: false,
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.25]
+      }
+    }
+  },
+  props: ['props'],
+  mixins: [onScroll],
+  methods: {
+    handleClick (i) {
+      if (this.activeTab === i) {
+        this.open = false
+        this.activeTab = null
+      } else {
+        this.open = true
+        this.activeTab = i
+      }
+    }
+  }
+}
+</script>
--- src/pages/treatments/treatments.vue
+++ src/pages/treatments/treatments.vue
@@ -1,6 +1,7 @@
 <template lang="pug" src="./treatments.pug"></template>
 
 <script>
+import Accordian from 'custom/custom-treatments-accordian/custom-treatments-accordian'
 import Hero from 'components/hero/hero-pages/hero-pages'
 import Sections from 'custom/custom-treatments-sections/custom-treatments-sections'
 
@@ -11,6 +12,7 @@ export default {
     }
   },
   components: {
+    Accordian,
     Hero,
     Sections
   }
